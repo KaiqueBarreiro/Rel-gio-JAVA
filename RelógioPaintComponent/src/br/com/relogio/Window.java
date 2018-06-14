8964907f7748d58695875d9d01fc63f9670ca772
E803962 <E803962@P010515.copel.nt>
2018-06-14 18:14:42 -0300

    commit relogio PaintComponent

--- RelógioPaintComponent/src/br/com/relogio/Window.java
+++ RelógioPaintComponent/src/br/com/relogio/Window.java
@@ -3,6 +3,7 @@ package br.com.relogio;
 import javax.swing.JFrame;
 
 public class Window extends JFrame {
+	
 	private static final long serialVersionUID = 1L;
 	private Paint relógio = new Paint();
 	private JFrame janela = new JFrame("Clock");;
